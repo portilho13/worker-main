87be0d1a2723bc1d21ec125dea1e898bc701afac
portilho13 <junior.portilho2005@gmail.com>
2024-07-26 18:27:35 +0100

    added hability to start connection with server when new server added

--- api/api.go
+++ api/api.go
@@ -2,10 +2,16 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/portilho13/worker-main/tunnel"
 )
 
 func Api(ip string, SERVERS_TCP_IP *[]string) error {
+
+	fmt.Println("Starting API")
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /servers", func(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +26,11 @@ func Api(ip string, SERVERS_TCP_IP *[]string) error {
 		id := r.PathValue("id")
 
 		*SERVERS_TCP_IP = append(*SERVERS_TCP_IP, id)
+
+		err := tunnel.ConnectToClient(ip)
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "text/plain")
